3459da39209a4503871f2775fcbfd397f9ba90ed
joechesney <33613119+joechesney@users.noreply.github.com>
2018-06-28 09:18:49 -0500

    made intro files and learned to use the bracket syntax to pass in a variable. its definitely not as cool as jsx, but itll do i guess

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const helloWorld = new Vue({
+  el: '#helloVue',
+  data: {
+    title: "HELLO WORLD!!!!",
+    message: "This is a message from Vue"
+  }
+});
+
+const example = new Vue({
+  el: '#example',
+  data: {
+    title: "Hello Chewie",
+    message: "have a catty day",
+    name: "Joe Chesney"
+  }
+});
