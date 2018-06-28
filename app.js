364f0edc9e65474fa9864ad8db538b157fcc01c9
joechesney <33613119+joechesney@users.noreply.github.com>
2018-06-28 11:09:56 -0500

    added a click event using the vue way

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 
+/*
+  SHORTHANDS
+
+  v-bind
+  <img v-bind:src="img.src" v-bind:alt="img.src">
+  same as
+  <img :src="img.src" :alt="img.src">
+
+  v-on
+  <h1 v-on:click="sayHello">{{title}}</h1>
+  same as
+  <h1 @click="sayHello">{{title}}</h1>
+
+
+*/
+
+
 const helloWorld = new Vue({
   el: '#helloVue',
   data: {
@@ -12,6 +29,24 @@ const example = new Vue({
   data: {
     title: "Hello Chewie",
     message: "have a catty day",
-    name: "Joe Chesney"
+    name: "Joe Chesney",
+    img: {
+      src: 'https://placeimg.com/200/200/animals',
+      alt: 'lil animal'
+    }
+  }
+});
+
+const book = new Vue({
+  el: "#book",
+  data: {
+    title: 'The Sirens of Titan',
+    author: 'Kurt Vonnegut',
+    summary: 'This is a summary',
+  },
+  methods: {
+    sayHello: function() {
+      alert(this.title)
+    }
   }
 });
